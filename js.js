525be97645aa37cc591ed6f87975b0c48fe13faf
Artem Vasylyshyn <artem.kievskaia.rus@gmail.com>
2022-08-17 17:57:31 +0300

    сменил переменную deposit

---
+++ js.js
@@ -0,0 +1,8 @@
+const money = 10;
+const income = 15;
+const addExpenses = 3;
+const deposit = 42;
+const mission = 20;
+const period = 5;
+alert('Жми "ок"');
+console.log(money + addExpenses + deposit - income - mission - period);
