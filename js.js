3fdb23ddb74669f1e3bfc3e79a45f0f5664a42e4
Artem Vasylyshyn <artem.kievskaia.rus@gmail.com>
2022-08-29 22:51:53 +0300

    добавил переменную и присвоил значение дохода за мес

--- js.js
+++ js.js
@@ -9,3 +9,5 @@ console.log(addExpenses.length);
 console.log('Период равен ' + period + ' месяцев');
 console.log('Цель заработать ' + mission + '$');
 console.log(addExpenses.toLowerCase(), addExpenses.split(', '));
+const budgetDay = money / 30; // дневной бюджет
+console.log(budgetDay);
