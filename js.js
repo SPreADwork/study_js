4a664adc50ac1cba1a1a3f654885d9a15ea27a8e
Artem Vasylyshyn <artem.kievskaia.rus@gmail.com>
2022-08-29 22:48:54 +0300

    используя методы вывел в консоль значения переменных

--- js.js
+++ js.js
@@ -1,6 +1,11 @@
 const money = 5000; // доход за месяц
 const income = 'Фриланс'; // доп. доход
-const addExpenses = 'коммуналка, транспорт, продукты, отдых'; // расходы
+const addExpenses = 'Коммуналка, транспорт, продукты, отдых'; // расходы
 const deposit = new Boolean(true);
 const mission = 200000; // запланируванная сумма
 const period = 12;
+console.log(typeof(money), typeof(income), typeof(deposit));
+console.log(addExpenses.length);
+console.log('Период равен ' + period + ' месяцев');
+console.log('Цель заработать ' + mission + '$');
+console.log(addExpenses.toLowerCase(), addExpenses.split(', '));
