25691386d4b6ffd1885ccb3d843e857757aa5aac
Artem Vasylyshyn <artem.kievskaia.rus@gmail.com>
2022-09-08 13:33:47 +0300

    Добавил пременные(expenses1, expenses2, amount1, amount2) с вопросами

--- js.js
+++ js.js
@@ -1,24 +1,16 @@
-// const money = prompt('Ваш месячный доход?'); // доход за месяц
-// const income = 'Фриланс'; // доп. доход
-// const addExpenses = prompt('Перечислите возможные расходы за рассчитываемый период через запятую'); // расходы
-
+const money = prompt('Ваш месячный доход?'); // доход за месяц
+const income = 'Фриланс'; // доп. доход
+const addExpenses = prompt('Перечислите возможные расходы за рассчитываемый период через запятую'); // расходы
 // 4) Спросить у пользователя “Есть ли у вас депозит в банке?” и сохранить данные в переменной deposit (булево значение true/false)
 // вариант 1
-// const deposit = confirm('Есть ли у вас депозит в банке?');
-// Вариант 2
-const deposit = prompt('Есть ли у вас депозит в банке?', 'да / нет');
-if (deposit == 'да') {
-   deposit = true;
-} else {
-    deposit = false;
-}
-console.log(deposit);
-// const mission = 200000; // запланируванная сумма
-// const period = 12;
-// console.log(typeof(money), typeof(income), typeof(deposit));
-// console.log(addExpenses.length);
-// console.log('Период равен ' + period + ' месяцев');
-// console.log('Цель заработать ' + mission + '$');
-// console.log(addExpenses.toLowerCase(), addExpenses.split(', '));
-// const budgetDay = money / 30; // дневной бюджет
-// console.log(budgetDay);
+const deposit = confirm('Есть ли у вас депозит в банке?');
+
+const mission = 200000; // запланируванная сумма
+const period = 12;
+console.log(typeof(money), typeof(income), typeof(deposit));
+console.log(addExpenses.length);
+console.log('Период равен ' + period + ' месяцев');
+console.log('Цель заработать ' + mission + '$');
+console.log(addExpenses.toLowerCase(), addExpenses.split(', '));
+const budgetDay = money / 30; // дневной бюджет
+console.log(budgetDay);
