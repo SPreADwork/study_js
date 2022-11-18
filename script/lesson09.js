b426ed17818044fcbdede347894c2fa0152c805e
Artem Vasylyshyn <artem.kievskaia.rus@gmail.com>
2022-11-19 00:52:08 +0200

    первые 3 переменных

---
+++ script/lesson09.js
@@ -0,0 +1,5 @@
+'use strict';
+
+let buttonCount = document.getElementById('start');
+let buttonPlusInc = document.getElementsByTagName('button')[0];
+let buttonPlusExp = document.getElementsByTagName('button')[1];
