bb4f49abb686c5235b263e647c81e36fafba6da4
Artem Vasylyshyn <artem.kievskaia.rus@gmail.com>
2022-11-23 00:20:18 +0200

    левая сторона

--- script/lesson09.js
+++ script/lesson09.js
@@ -4,15 +4,31 @@ let buttonCount = document.getElementById('start');
 let buttonPlusInc = document.getElementsByTagName('button')[0];
 let buttonPlusExp = document.getElementsByTagName('button')[1];
 let box = document.querySelector('#deposit-check');
+let imputAddIncome1 = document.querySelectorAll('.additional_income-item')[0];
+let imputAddIncome2 = document.querySelectorAll('.additional_income-item')[1];
+// Rights
+let impBudgetMonthRes = document.querySelector('.budget_month-value');
+let impBudgetDayRes = document.querySelector('.budget_day-value');
+let impExpensesMonthRes = document.querySelector('.expenses_month-value');
+let impAddItionalIncomeRes = document.querySelector('.additional_income-value');
+let impAddItionalExpensesRes = document.querySelector('.additional_expenses-value');
+let impIncomePeriodRes = document.querySelector('.income_period-value');
+let impTargetMonthRes = document.querySelector('.target_month-value');
+// Left
+let impSalaryAmount = document.querySelector('.salary-amount');
+let impIncomeTitle = document.querySelector('div.income-items input[class=income-title]');
+let impIncomeAmount = document.querySelector('div.income-items input[class=income-amount]');
+let impExpensesTitle = document.querySelector('div.expenses-items input[class=expenses-title]');
+let impExpensesAmount = document.querySelector('div.expenses-items input[class=expenses-amount]');
+let impAddItionalExpenses = document.querySelector('.additional_expenses-item');
+let impDepositAmount = document.querySelector('.deposit-amount');
+let impDepositPercent = document.querySelector('.deposit-percent');
+let impTargetAmount = document.querySelector('.target-amount');
+let impPeriodSelect = document.querySelector('.period-select');
+console.log(impPeriodSelect);
 
-let imputAddIncome = document.querySelectorAll('.additional_income-item');
-let impBudgetMonth = document.querySelector('.budget_month-value');
-let impBudgetDay = document.querySelector('.budget_day-value');
-let impExpensesMonth = document.querySelector('.expenses_month-value');
-let impAddItionalIncome = document.querySelector('.additional_income-value');
-let impAddItionalExpenses = document.querySelector('.additional_expenses-value');
-let impIncomePeriod = document.querySelector('.income_period-value');
-let impTargetMonth = document.querySelector('.target_month-value');
+
+
 
 
 
