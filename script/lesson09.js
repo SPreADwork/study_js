02a9a76b1cacf7720aa1bd060d6c676faba64c4b
Artem Vasylyshyn <artem.kievskaia.rus@gmail.com>
2022-11-22 20:12:01 +0200

    правая сторона

--- script/lesson09.js
+++ script/lesson09.js
@@ -3,3 +3,16 @@
 let buttonCount = document.getElementById('start');
 let buttonPlusInc = document.getElementsByTagName('button')[0];
 let buttonPlusExp = document.getElementsByTagName('button')[1];
+let box = document.querySelector('#deposit-check');
+
+let imputAddIncome = document.querySelectorAll('.additional_income-item');
+let impBudgetMonth = document.querySelector('.budget_month-value');
+let impBudgetDay = document.querySelector('.budget_day-value');
+let impExpensesMonth = document.querySelector('.expenses_month-value');
+let impAddItionalIncome = document.querySelector('.additional_income-value');
+let impAddItionalExpenses = document.querySelector('.additional_expenses-value');
+let impIncomePeriod = document.querySelector('.income_period-value');
+let impTargetMonth = document.querySelector('.target_month-value');
+
+
+
