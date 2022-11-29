4e6686dab1cf77a81ae509e03e92b5f1e4bd02ba
Artem Vasylyshyn <artem.kievskaia.rus@gmail.com>
2022-11-30 00:34:03 +0200

    заменил задний фон

--- урок 10/js10.js
+++ урок 10/js10.js
@@ -5,3 +5,5 @@ let book = document.querySelectorAll('.book');
 books[0].insertBefore(book[1], book[0]);
 books[0].appendChild(book[2]);
 books[0].insertBefore(book[4], book[3]);
+// заменить задний фон
+document.querySelector('body').style.backgroundImage = 'url(./image/profile.jpg)';
