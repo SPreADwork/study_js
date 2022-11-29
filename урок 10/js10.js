b26acefe861aff2286f2e8f84ff62e2ee86909e2
Artem Vasylyshyn <artem.kievskaia.rus@gmail.com>
2022-11-29 23:40:44 +0200

    исправил порядок книг

--- урок 10/js10.js
+++ урок 10/js10.js
@@ -1,1 +1,7 @@
 'use strict';
+
+let books = document.querySelectorAll('.books');
+let book = document.querySelectorAll('.book');
+books[0].insertBefore(book[1], book[0]);
+books[0].appendChild(book[2]);
+books[0].insertBefore(book[4], book[3]);
