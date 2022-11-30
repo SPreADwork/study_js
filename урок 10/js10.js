d8fa0b99fb0ed5626c36345fced938e589da8a90
Artem Vasylyshyn <artem.kievskaia.rus@gmail.com>
2022-11-30 23:41:55 +0200

    восстановил порядок глав

--- урок 10/js10.js
+++ урок 10/js10.js
@@ -11,4 +11,15 @@ document.querySelector('body').style.backgroundImage = 'url(./image/profile.jpg)
 book[4].querySelector('a').textContent = 'Книга 3. this и Прототипы Объектов';
 // удалить рекламу
 document.querySelector('.adv').remove();
-
+// восстановить порядок главы №2
+let chapters = document.querySelectorAll('ul');
+let topic2 = chapters[1].querySelectorAll('li');
+chapters[1].insertBefore(topic2[8], topic2[4]);
+chapters[1].insertBefore(topic2[6], topic2[8]);
+chapters[1].insertBefore(topic2[2], topic2[10]);
+// восстановить порядок главы №5
+let topic5 = chapters[4].querySelectorAll('li');
+chapters[4].insertBefore(topic5[2], topic5[6]);
+chapters[4].insertBefore(topic5[9], topic5[3]);
+chapters[4].insertBefore(topic5[5], topic5[8]);
+console.log(topic5);
